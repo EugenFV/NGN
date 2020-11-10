a70fe523cc4ddea7a5bfffce71bd3a7e7a09a7e1
EugenFV <eugen.ferafontov@gmail.com>
2020-11-10 22:55:54 +0200

    [Temp] Examples

---
+++ Assets/_TempScripts/MoveWayPoints.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWayPoints : MonoBehaviour
+{
+    [SerializeField] private Transform _point00 = null;
+    [SerializeField] private Transform _point01 = null;
+    [SerializeField] private Transform _currentTarget = null;
+
+    [SerializeField] private float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentTarget == null)
+            return;
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget.position, step);
+        if (Vector3.Distance(transform.position, _currentTarget.position) < 0.001f)
+        {
+            if (_currentTarget == _point00)
+            {
+                _currentTarget = _point01;
+            }
+            else
+            {
+                _currentTarget = _point00;
+            }
+        }
+    }
+}
---
+++ Assets/_TempScripts/TargetLookAt.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLookAt : MonoBehaviour
+{
+    [SerializeField] private Transform _target = null;
+    // Start is called before the first frame update
+    private bool _isPress = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (null == _target)
+            return;
+
+
+        if (_isPress)
+        {
+            Vector3 targetDirection = _target.position - transform.position;
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+            Quaternion lookAt = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * 1000.0f);
+            //transform.rotation = lookAt;
+            transform.rotation = targetRotation;
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    #region Implementation
+
+    void OnGUI()
+    {
+        //return;
+        Event e = Event.current;
+        if (e.type == EventType.KeyDown)
+        {
+            if (e.keyCode == KeyCode.Z)
+            {
+                _isPress = true;
+            }
+        }
+
+        if (e.type == EventType.KeyUp)
+        {
+            if (e.keyCode == KeyCode.Z)
+            {
+                _isPress = false;
+            }
+        }
+    }
+
+    #endregion
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
